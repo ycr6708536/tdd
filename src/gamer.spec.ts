d83eb43b88415e1a3a1f0626c807d62e20a01332
闫超仁 <yanchaoren@xiaomi.com>
2019-08-11 10:52:15 +0800

    test case

--- src/gamer.spec.ts
+++ src/gamer.spec.ts
@@ -1,11 +1,10 @@
 import { Gamer } from './gamer'
 
-describe('gamer test ', () => {
-  let keys = [1, 3, 5, 15]
-  let says = ['1', 'Fizz', 'Buzz', 'FizzBuzz']
-  keys.forEach((item: number, idx: number) => {
-    it('say 1 with 1', () => {
-      expect(Gamer.say(item)).toBe(says[idx])
-    })
-  })
-})
+describe.each([[1, '1'], [3, 'Fizz'], [5, 'Buzz'], [15, 'FizzBuzz']])(
+  '(%d) should',
+  (input: number, expected: string) => {
+    test(`say ${expected}`, () => {
+      expect(Gamer.say(input)).toBe(expected);
+    });
+  }
+);
